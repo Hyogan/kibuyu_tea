f138595da24487557389ba1b69cfd7e05f0ef48d
Hyogan <tiaghojeux@gmail.com>
2024-02-19 06:16:34 +0100

    adding some animations , the products types and dealing with the favicon

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
+
 const header = document.querySelector(".header-side");
-const hamburger = document.querySelector("#menu-btn-side");
+const hamburger = document.querySelector(".hamburger");
 const navbar = document.querySelector("nav")
 hamburger.addEventListener("click",()=>{
     // alert("Hellow world")
 
     header.classList.toggle("open");
+    hamburger.classList.toggle("menu-btn-side");
+    // if(hamburger.id)
 
 });
